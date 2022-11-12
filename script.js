90ea8a7629f7cc29b72b6b1be32e47aeb596419f
Solmaz-Salehzadeh <solmaz.salehzadeh1369@gmail.com>
2022-11-12 22:24:52 +0330

    question2

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function setterGenerator(prop) {
-    return function (name) {
-      user[prop] = name;
-      return user;
-    };
-  }
-  
-  const user = {};
-  const setterGeneratorBind = setterGenerator.bind(user)
-  const nameSetter = setterGeneratorBind("name");
-  nameSetter("solmaz");
-  console.log(user);
+
+function elemCreator(tagName, callback) { 
+    const element = document.createElement(tagName); 
+    const innerText = document.createTextNode("Heeeey"); 
+    element.appendChild(innerText); 
+    document.body.appendChild(element); 
+   
+    callback(element); 
+  } 
+   
+  elemCreator("div", function (elem) { 
+    elem.style.backgroundColor = "#f28073"; 
+    elem.style.padding="100px"
+    elem.style.width="fit-content"
+    elem.style.borderRadius="16PX"
+  });
